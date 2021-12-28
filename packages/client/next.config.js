120e47e22f4d983c13d5e838b73979433a5cf42a
jpressley1 <93457986+jpressley1@users.noreply.github.com>
2021-12-27 20:34:10 -0500

    Update next.config.js

--- packages/client/next.config.js
+++ packages/client/next.config.js
@@ -2,7 +2,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
     env: {
-        contentApi: 'labs.cdn.content.amplience.net'
+        contentApi: 'jesstest.cdn.content.amplience.net'
     },
     poweredByHeader: false,
     target: 'serverless'
